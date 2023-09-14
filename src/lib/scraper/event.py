0031575028a705c3bfecb020fa7333eebba67569
guilherme <guilherme.g.sampaio@gmail.com>
2023-09-14 16:52:55 +0100

    feat: finalized docs

--- src/lib/scraper/event.py
+++ src/lib/scraper/event.py
@@ -5,27 +5,62 @@ from datetime import datetime
 
 @dataclass(frozen=True)
 class Location:
+    """
+    This class represents the location of an event.
+    :param building: Building where the event is taking place.
+    :type building: str
+    :param campus: Campus where the event is taking place.
+    :type campus: str
+    :param room: Room where the event is taking place.
+    :type room: str
+    """
     building: str
     campus: str
     room: str
 
     @classmethod
     def from_string(cls, string: str) -> "Location":
+        """
+        Class method that builds a Location from a given string.
+        :param string: Given string to be parsed into a Location object.
+        :type string: str
+        :return: Location object from the string.
+        :rtype: Location
+        """
         campus, building, room = tuple(string.replace(" ", "").split("-"))
         return cls(building=building.replace("Edificio", "CP"), campus=campus, room=room)
 
-    def __str__(self):
+    def __str__(self) -> str:
+        """
+        String representation of the class.
+        """
         return f"{self.campus} - {self.building} {self.room}"
 
 
 @dataclass(frozen=True)
 class ScheduleBody:
+    """
+    This class represents the body of an event.
+    :param name: Event name.
+    :type name: str
+    :param location: Location object that represents where the event is taking place.
+    :type location: Location
+    :param shift: Shift for the event.
+    :type shift: str
+    """
     name: str
     location: Location
     shift: str
 
     @classmethod
     def from_string(cls, body: str) -> "ScheduleBody":
+        """
+        Class method that builds a ScheduleBody from a given string.
+        :param body: Given string to be parsed into a ScheduleBody object.
+        :type body: str
+        :return: ScheduleBody object from the string.
+        :rtype: ScheduleBody
+        """
         matches = re.match(r"(.*)\[(.*)](.*)", body.replace("\n", ""))
         course_name, location, shift = matches.groups()
 
@@ -34,11 +69,25 @@ class ScheduleBody:
         return cls(name=course_name.strip().lower(), location=location_obj, shift=shift.strip())
 
     def __str__(self):
+        """
+        String representation of the class.
+        """
         return f"{self.name.title()}\n{self.location} - {self.shift}"
 
 
 @dataclass(frozen=True)
 class ScheduleEvent:
+    """
+    This class represents an event.
+    :param body: Event details.
+    :type body: ScheduleBody
+    :param starts_at: Starting that of the event.
+    :type starts_at: datetime
+    :param duration: Duration of the event.
+    :type duration: datetime
+    :param weekday: Weekday of the event.
+    :type weekday: str
+    """
     body: ScheduleBody
     starts_at: datetime
     duration: datetime
@@ -46,6 +95,19 @@ class ScheduleEvent:
 
     @classmethod
     def build(cls, body: str, starts_at: datetime, duration: datetime, weekday: str) -> "ScheduleEvent":
+        """
+        Given a string and some details (duration, starting time, weekday), this method builds an event.
+        :param body: Event details as string.
+        :type body: str
+        :param starts_at: Starting that of the event.
+        :type starts_at: datetime
+        :param duration: Duration of the event.
+        :type duration: datetime
+        :param weekday: Weekday of the event.
+        :type weekday: str
+        :return: ScheduleEvent object representing the event.
+        :rtype: ScheduleEvent
+        """
         return cls(
             body=ScheduleBody.from_string(body),
             starts_at=starts_at,
@@ -54,4 +116,7 @@ class ScheduleEvent:
         )
 
     def __str__(self):
+        """
+        String representation of the class.
+        """
         return f"{self.body}\n{self.starts_at}\n{self.duration}\n"
--- src/lib/scraper/parser.py
+++ src/lib/scraper/parser.py
@@ -9,38 +9,76 @@ from src.lib.scraper.schedule import Schedule
 
 
 class ScheduleParser:
+    """
+    This class provides the methods necessary to parse the html raw scraped content into a Schedule/ScheduleEvent.
+    """
 
     @staticmethod
     def _parse_weekdays(context: BeautifulSoup) -> list[str]:
-
+        """
+        Method that parses the present weekdays on the schedule.
+        :param context: The current 'container' that is being worked on.
+        :type context: BeautifulSoup
+        :return: List containing the weekdays used.
+        :rtype: list[str]
+        """
         weekday_table = (context.find_all("table", class_="rsHorizontalHeaderTable"))[0]
         return [weekday.text.title() for weekday in weekday_table.find_all("a")]
 
     @staticmethod
     def _parse_time(context: BeautifulSoup) -> datetime:
-
+        """
+        Method that parses the hours present in a row.
+        :param context: The current 'container' that is being worked on.
+        :type context: BeautifulSoup
+        :return: The time in the form of a datetime object.
+        :rtype: datetime
+        """
         time_table = (context.find_all("table", class_="rsVerticalHeaderTable"))[0]
         time_str: str = time_table.find_next("div").text.title().strip()
         return datetime.strptime(time_str, "%H:%M")
 
     @staticmethod
     def _add_to_time(current_time: datetime, minutes: int) -> datetime:
+        """
+        Method that adds minutes to a defined time.
+        :param current_time: Time to be added to.
+        :type current_time: datetime
+        :param minutes: Minutes to add on current_time.
+        :type minutes: int
+        :return: A datetime object of the addition.
+        :rtype: datetime
+        """
         return current_time + timedelta(minutes=minutes)
 
     @staticmethod
     def _get_duration(style_string: str) -> datetime:
-
+        """
+        Given a style css string, this method extracts the height of the container and based on that calculates the
+        duration of it. Height above 200 means a duration of 2 hours, height bellow 200 means a duration of 1 hour.
+        :param style_string: String containing the css string of the container.
+        :type style_string: str
+        :return: The duration as a datetime.
+        :rtype: datetime
+        """
         block_height_match: re.Match = re.search(r"height:(\d+)px", style_string)
-
-        if int(block_height_match.group(1)) > 200:
-            duration: str = "2:00"
-
-        else:
-            duration: str = "1:00"
+        duration: str = "2:00" if int(block_height_match.group(1)) > 200 else "1:00"
 
         return datetime.strptime(duration, "%H:%M")
 
     def _parse_blocks(self, context: ResultSet, current_time: datetime, weekday: str) -> list[ScheduleEvent]:
+        """
+        Each row of the schedule can and will have multiple events, this method parses those events into
+        ScheduleEvent objects.
+        :param context: Current 'block' we are in.
+        :type context: ResultSet
+        :param current_time: Current time of events.
+        :type current_time: datetime
+        :param weekday: Current weekday.
+        :type weekday: str
+        :return: List of every parsed block inside the container.
+        :rtype: list[ScheduleEvent]
+        """
 
         events: list[ScheduleEvent] = []
 
@@ -52,7 +90,15 @@ class ScheduleParser:
 
         return events
 
-    def parse(self, content: str):
+    def parse(self, content: str) -> Schedule:
+        """
+        Given the raw source code of the scraped page, this method is responsible for parsing every and each event
+        aggregating them onto a Schedule object.
+        :param content: The raw source code of the schedule page.
+        :type content: str
+        :return: The newly parsed schedule.
+        :rtype: Schedule
+        """
 
         soup: BeautifulSoup = BeautifulSoup(content, "lxml")
 
